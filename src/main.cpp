0a4ee2e2a3d77116ae8a46a6d5fd98d7fbccf72c
Marcus Klein <himself@kleini.org>
2019-02-03 20:59:20 +0100

    deep sleep is working but requires some workaround

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,38 @@
 #define FW_NAME "esp12e"
-#define FW_VERSION "0.1.0"
+#define FW_VERSION "0.2.0"
 
 #include "Homie.h"
 
 const int PIN_LED = 2;
 
+void onHomieEvent(const HomieEvent& event) {
+  switch(event.type) {
+    case HomieEventType::MQTT_READY:
+      Homie.getLogger() << "MQTT connected, preparing for deep sleep..." << endl;
+      Homie.prepareToSleep();
+      break;
+    case HomieEventType::READY_TO_SLEEP:
+      Homie.getLogger() << "Ready to sleep" << endl;
+      Homie.doDeepSleep(60*1000000, RFMode::RF_DISABLED);
+      break;
+    default:
+      break;
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   Serial << endl << endl;
 
+  // WiFi does not connect properly after being woken up from deep sleep.
+  // Disconnecting solves this issue.
+  WiFi.disconnect();
+
   Homie_setFirmware(FW_NAME, FW_VERSION);
   Homie.setLedPin(PIN_LED, LOW);
   Homie.disableResetTrigger();
+
+  Homie.onEvent(onHomieEvent);
 
   Homie.setup();
 }
