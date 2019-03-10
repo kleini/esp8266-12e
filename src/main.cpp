c1a26fee60b8518cac6dbadb52a5e8f7196772f9
Marcus Klein <himself@kleini.org>
2019-03-10 19:36:09 +0100

    use timer for proper sending of node properties

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,27 @@
 #define FW_NAME "esp8266-12e"
-#define FW_VERSION "0.4.0"
+#define FW_VERSION "0.5.0"
 
 #include "Homie.h"
 #include "DHT22Node.hpp"
 #include "DS18B20Node.hpp"
+#include "Timer.h"
 
 const int PIN_LED = 2;
 const int PIN_DHT = 13;
 const int PIN_DS = 4;
 
-DS18B20Node ds18b20("ds18b20", PIN_DS, 50);
-DHT22Node dht22("dht22", PIN_DHT, 50);
+DS18B20Node ds18b20("ds18b20", PIN_DS, 60);
+DHT22Node dht22("dht22", PIN_DHT, 60);
+Timer t;
+
+void prepareSleep() {
+  Homie.prepareToSleep();
+}
 
 void onHomieEvent(const HomieEvent& event) {
   switch(event.type) {
     case HomieEventType::MQTT_READY:
-      Homie.prepareToSleep();
+      t.after(100, prepareSleep);
       break;
     case HomieEventType::READY_TO_SLEEP:
       Homie.doDeepSleep(60*1000000);
@@ -31,12 +37,11 @@ void setup() {
 
   pinMode(PIN_LED, OUTPUT);
   pinMode(PIN_DHT, INPUT);
+  pinMode(PIN_DS, INPUT_PULLUP);
 
   // WiFi does not connect properly after being woken up from deep sleep.
   // Trying different things to solve this.
-  // WiFi.disconnect();
   WiFi.forceSleepWake();
-  // WiFi.mode(WiFiMode::WIFI_OFF);
 
   Homie_setFirmware(FW_NAME, FW_VERSION);
   Homie.setLedPin(PIN_LED, LOW);
@@ -49,4 +54,5 @@ void setup() {
 
 void loop() {
   Homie.loop();
+  t.update();
 }
